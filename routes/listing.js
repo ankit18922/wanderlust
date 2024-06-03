31134bfd226ccb0f65270604b245fa5affa1bf8b
spotify-clone <ankit18922@gmail.com>
2024-06-04 00:20:22 +0530

    Add Project Files

---
+++ routes/listing.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const {isLoggedIn, isOwner, validateListing, isReviewAuthor} = require("../middleware.js");
+const ListingController = require("../controllers/listings.js");
+const multer  = require('multer')
+const {storage} = require("../cloudConfig.js");
+const upload = multer({storage});
+
+
+router.route("/")
+     .get( wrapAsync(ListingController.index))
+
+     .post( 
+        isLoggedIn, 
+        //validateListing,
+        upload.single("listing[image]"), 
+        validateListing,
+        wrapAsync( ListingController.createListing));
+    
+     
+// New Route
+ router.get("/new", 
+          isLoggedIn, 
+          ListingController.renderNewForm
+);
+
+router.route("/:id")
+     .get( wrapAsync(ListingController.showListing))
+
+     .put( 
+     isLoggedIn,
+     isOwner,
+     upload.single("listing[image]"),
+     validateListing, 
+     wrapAsync(ListingController.updateListing))
+
+     .delete(
+     isLoggedIn,
+     isOwner, 
+     wrapAsync(ListingController.destroyListing));
+ 
+ // Edit Route
+ router.get("/:id/edit", 
+       isLoggedIn, 
+       isOwner,
+       wrapAsync( ListingController.renderEditForm));
+
+
+module.exports = router;
